2c6fbc8341f139ef05052b9d957eb984d809b27d
Kai Aurelien Zhu <kai.aurelien.zhu@gmail.com>
2021-03-20 22:24:36 +1100

    centreline

--- src/VicGovResiEnvelope.cs
+++ src/VicGovResiEnvelope.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System;
 using System.Linq;
 
+
 namespace VicGovResiEnvelope
 {
       public static class VicGovResiEnvelope
@@ -21,12 +22,21 @@ namespace VicGovResiEnvelope
             var siteElement = siteModel.AllElementsOfType<Site>().First();
             var perimeter = siteElement.Perimeter.Offset(-input.Setback);
             var profile = new Profile(perimeter);
+            List<Line> lotBoundarySegments = profile.Segments();
+            List<Vector3> midPoints = lotBoundarySegments.Select(i => i.PointAt(0.5)).ToList();
+            
+            // Get plot centreline TO DO: Sort by segment lengths
+            var centreLine = new Line(midPoints[0], midPoints[2]);
+            var modelLine = new ModelCurve(centreLine);
+
             var mass =  new Mass(profile, 10);
-            var modelCurves = perimeter.Select(p => new ModelCurve(p));
+            var modelCurves = perimeter.Select(i => new ModelCurve(i));
+
 
             // Generate outputs
             var output = new VicGovResiEnvelopeOutputs();
             output.Model.AddElements(modelCurves);
+            output.Model.AddElement(modelLine);
             output.Model.AddElements(mass);
             return output;
         }
